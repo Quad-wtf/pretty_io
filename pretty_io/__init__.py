b1c680cb0bad10b3ca8096a51f27d8d32acb2ed0
Quad-wtf <quad.business.inquirities@gmail.com>
2025-08-13 22:57:24 +0200

    Add files via upload

---
+++ pretty_io/__init__.py
@@ -0,0 +1,2 @@
+from .pretty_io import PrintPretty, InputPretty, InputOutputPretty, PrintPossibilities, STYLES, COLORS
+__all__ = ["PrintPretty", "InputPretty", "InputOutputPretty", "PrintPossibilities", "STYLES", "COLORS"]
